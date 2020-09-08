0a6fc45917b5efbcf7cc984bc006bc54ffaed2bd
Austin Scott <austin.scott18@gmail.com>
2020-09-08 19:29:17 -0400

    add gatsby-plugin-google-fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,5 +44,17 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `limelight`,
+          `source sans pro\:300,400,400i,700`,
+          `roboto mono`,
+          `muli\:400,400i,700,700i`,
+        ],
+        display: "swap",
+      },
+    },
   ],
 }
